692cf804b0095ccdbdafccefd6037d2ee96a5b8a
Eric Chen <echen9898@gmail.com>
2020-09-25 15:08:58 -0700

    (minor)[Fix]: Took out last ReLU in forward model
    Why: Output of the forward model is a predicted image (probability distribution). This is then passed
    through a sigmoid in the BCELoss function. This sigmoid takes (-inf, inf) inputs and maps them to (0,1).
    Mapping the last layer through ReLU means your probabilities are always above 0.5, which is incorrect.
    
    ---------------------------------------------------------------------------------------------------------
    
    How: The last ReLU is taken out of the forward model. The forward model now resembles ICM's inverse
    model - just two linear layers with a ReLU in between. BCELoss with logits is used, as it's more
    numerically stable than BCELoss with a sigmoid function applied separately.
    
    ---------------------------------------------------------------------------------------------------------
    
    Tags:
    
    References:

--- rlpyt/models/curiosity/ndigo.py
+++ rlpyt/models/curiosity/ndigo.py
@@ -24,8 +24,7 @@ class NdigoForward(nn.Module):
 
         self.model = nn.Sequential(nn.Linear(feature_size + action_size, 64),
                                    nn.ReLU(),
-                                   nn.Linear(64, output_size),
-                                   nn.ReLU())
+                                   nn.Linear(64, output_size))
 
     def forward(self, belief_states, action_seqs):
         predicted_states = self.model(torch.cat([belief_states, action_seqs], 2))
@@ -129,36 +128,35 @@ class NDIGO(torch.nn.Module):
         
         # make forward model predictions
         predicted_states = self.forward_model[self.horizon-1](belief_states, actions[:T-self.horizon].detach()).view(-1, B, img_shape[0]*img_shape[1]*img_shape[2]) # (T-k, B, 75)
-        predicted_states = torch.sigmoid(predicted_states)
         true_obs = observations[self.horizon:].view(-1, *predicted_states.shape[1:])
 
         # DEBUGGING
-        path = '/curiosity_baselines/results/ppo_Deepmind5Room-v0/run_0/images'
-        if self.pred_vis_counter == 30:
-            self.pred_vis_counter = 0
-            if not os.path.isdir(path):
-                os.mkdir(path)
-            ep_num = len(os.listdir(path))
-            os.mkdir(path + '/ep_{}'.format(ep_num))
-            pred = predicted_states.detach().clone().data.numpy()
-            true = true_obs.detach().clone().data.numpy()
-            pred = np.reshape(pred[10, 0], (3, 5, 5))
-            true = np.reshape(true[10, 0], (3, 5, 5))
+        # path = '/curiosity_baselines/results/ppo_Deepmind5Room-v0/run_0/images'
+        # if self.pred_vis_counter == 30:
+        #     self.pred_vis_counter = 0
+        #     if not os.path.isdir(path):
+        #         os.mkdir(path)
+        #     ep_num = len(os.listdir(path))
+        #     os.mkdir(path + '/ep_{}'.format(ep_num))
+        #     pred = predicted_states.detach().clone().data.numpy()
+        #     true = true_obs.detach().clone().data.numpy()
+        #     pred = np.reshape(pred[10, 0], (3, 5, 5))
+        #     true = np.reshape(true[10, 0], (3, 5, 5))
 
-            print(pred)
-            print('-'*100)
-            print(true)
-            print('#'*100)
-            for i in range(3):
-                pred_img = Image.fromarray((pred[i]*500).astype(np.uint8), 'L')
-                true_img = Image.fromarray((true[i]*500).astype(np.uint8), 'L')
-                pred_img.save(path + '/ep_{}/pred_{}.jpg'.format(ep_num, i))
-                true_img.save(path + '/ep_{}/true_{}.jpg'.format(ep_num, i))
-        else:
-            self.pred_vis_counter += 1
+        #     print(pred)
+        #     print('-'*100)
+        #     print(true)
+        #     print('#'*100)
+        #     for i in range(3):
+        #         pred_img = Image.fromarray((pred[i]*500).astype(np.uint8), 'L')
+        #         true_img = Image.fromarray((true[i]*500).astype(np.uint8), 'L')
+        #         pred_img.save(path + '/ep_{}/pred_{}.jpg'.format(ep_num, i))
+        #         true_img.save(path + '/ep_{}/true_{}.jpg'.format(ep_num, i))
+        # else:
+        #     self.pred_vis_counter += 1
 
         # generate losses
-        losses = nn.functional.binary_cross_entropy(predicted_states, true_obs.detach(), reduction='none')
+        losses = nn.functional.binary_cross_entropy_with_logits(predicted_states, true_obs.detach(), reduction='none')
         losses = torch.sum(losses, dim=-1)/losses.shape[-1] # average of each feature for each environment at each timestep (T, B, ave_loss_over_feature)
         
         # subtract losses to get rewards
@@ -195,12 +193,11 @@ class NDIGO(torch.nn.Module):
 
             # make forward model predictions for this predinorctor
             predicted_states = self.forward_model[k-1](belief_states[:T-k], actions[:T-k].detach()).view(-1, img_shape[0]*img_shape[1]*img_shape[2]) # (T-k, B, 75)
-            predicted_states = torch.sigmoid(predicted_states)
 
             # generate losses for this predictor
             true_obs = observations[k:].view(-1, *predicted_states.shape[1:]).detach()
 
-            loss += nn.functional.binary_cross_entropy(predicted_states, true_obs.detach(), reduction='mean')
+            loss += nn.functional.binary_cross_entropy_with_logits(predicted_states, true_obs.detach(), reduction='mean')
 
         return loss
 
